0d521d4052bca74019ba202d63a9fa77568accaf
Maxence Cottel <mcottel@nacon.fr>
2025-01-24 21:58:09 +0100

    Add guinevere picture

--- src/data/cars.ts
+++ src/data/cars.ts
@@ -13,7 +13,7 @@ export const cars = [
       "pv": 45
     },
     "prix": "225000",
-    "image": "https://imgur.com/a/i1v3TdM"
+    "image": "https://images.unsplash.com/photo-1737751469198-76a1394f5745?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     "nom": "Caliburn",

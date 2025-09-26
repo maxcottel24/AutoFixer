f4b987419579fa0bcd44af468789d10c3560187e
Maxence Cottel <mcottel@nacon.fr>
2025-09-26 15:44:17 +0200

    Design update

--- src/pages/Design.tsx
+++ src/pages/Design.tsx
@@ -9,16 +9,29 @@ export const DesignPage = () => {
       </p>
 
       {/* // add all Text variants here for demonstration */}
-      <Text variant="h1" className="mb-4">Heading 1</Text>
-      <Text variant="h2" className="mb-4">Heading 2</Text>
-      <Text variant="h3" className="mb-4">Heading 3</Text>
-      <Text variant="h4" className="mb-4">Heading 4</Text>
-      <Text variant="s1" className="mb-4">Subtitle 1: Lorem ipsum dolor sit amet.</Text>
-      <Text variant="s2" className="mb-4">Subtitle 2: Lorem ipsum dolor sit amet.</Text>
-      <Text variant="p1" className="mb-4">Paragraph 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Text>
-      <Text variant="p2" className="mb-4">Paragraph 2: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Text>
-      <Text variant="c1" className="mb-4">Caption 1: Lorem ipsum dolor sit amet.</Text>
-      <Text variant="c2" className="mb-4">Caption 2: Lorem ipsum dolor sit amet.</Text>
+      <div className="gap-4 ">
+      <Text variant="h1">Heading 1</Text>
+      <Text variant="h2">Heading 2</Text>
+      <Text variant="h3">Heading 3</Text>
+      <Text variant="h4">Heading 4</Text>
+      <Text variant="s1">Subtitle 1: Lorem ipsum dolor sit amet.</Text>
+      <Text variant="s2">Subtitle 2: Lorem ipsum dolor sit amet.</Text>
+      <Text variant="p1">Paragraph 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Text>
+      <Text variant="p2">Paragraph 2: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Text>
+      <Text variant="c1">Caption 1: Lorem ipsum dolor sit amet.</Text>
+      <Text variant="c2">Caption 2: Lorem ipsum dolor sit amet.</Text>
+      <Text variant="price">price</Text>
+      <Text variant="carPageName">carPageName</Text>
+      <Text variant="carPageBrand">carPageBrand</Text>
+      <Text variant="carPageClass">carPageClass</Text>
+      <Text variant="carPageTitleStats">carPageTitleStats</Text>
+      <Text variant="carPageStats">carPageStats</Text>
+      <Text variant="colorTitle">colorTitle</Text>
+      <Text variant="carPageDescription">carPageDescription' </Text>
+      <Text variant="weaponSystemTitle">weaponSystemTitle</Text>
+      <Text variant="weaponSystemTitleStats">weaponSystemTitleStats</Text>
+      <Text variant="weaponSystemSubtitle">weaponSystemSubtitle</Text>
+      </div>
     </div>
   );
 }
